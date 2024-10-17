65cb5e9f7684d006f08bf24ef1df5c9af1d35639
Antoine Boucher <antoine.bou13@gmail.com>
2024-10-17 23:37:00 +0000

    add github actions

--- components/sections/about/about.js
+++ components/sections/about/about.js
@@ -5,211 +5,134 @@ class AboutSection extends LitElement {
     :host {
       display: block;
       padding: 2rem;
-      background: linear-gradient(
-        135deg,
-        var(--accent-color-2),
-        var(--accent-color-3)
-      );
-      text-align: center;
       font-family: "Manrope", Arial, sans-serif;
-    }
-
-    section {
-      margin: 0 auto;
-      max-width: 900px;
-      padding: 2rem;
-      background-color: rgba(255, 255, 255, 0.1);
-      backdrop-filter: blur(5px);
-      border-radius: 15px;
-      box-shadow: 0 4px 15px rgba(0, 0, 0, 0.1);
-    }
-
-    h1 {
-      color: #001d31;
-      font-size: 2.5rem;
-      margin-bottom: 1.5rem;
-      font-weight: 700;
-    }
-
-    p {
-      color: #eef2f9;
-      font-size: 1.125rem;
-      line-height: 1.75;
-      max-width: 800px;
-      margin: 0 auto;
-      text-align: justify;
-    }
-
-    .button-container {
-      margin-top: 2rem;
-      display: flex;
-      justify-content: center;
-      gap: 1rem;
-    }
-
-    button {
-      background-color: #bd0029;
-      color: white;
-      padding: 0.75rem 1.5rem;
-      border-radius: 8px;
-      font-size: 1rem;
-      cursor: pointer;
-      border: none;
-      transition: background-color 0.3s ease, transform 0.2s ease;
-    }
-
-    button:hover {
-      background-color: #d8063b;
-      transform: scale(1.05); /* Added a slight scaling effect */
-    }
-
-    button:focus {
-      outline: 4px solid #d8063b;
-      outline-offset: 2px;
-    }
-
-    button:active {
-      background-color: #a90024; /* Darker color when clicked */
-    }
-
-    @media (max-width: 768px) {
-      h1 {
-        font-size: 2rem;
-      }
-
-      p {
-        font-size: 1rem;
-      }
-
-      button {
-        font-size: 0.875rem;
-        padding: 0.5rem 1rem;
-      }
     }
   `;
 
   render() {
     return html`
-      <section id="about" aria-labelledby="about-heading">
-        <h1 id="about-heading">About the Event</h1>
-        <p>
-          GRAPHQUON (formerly MOTOGRAPH) is the annual Quebec-Ontario
-          pre-SIGGRAPH workshop, organized by and for the major East-Canadian
-          computer graphics labs. It is an opportunity for students to present
-          their prospective submissions to a friendly, constructively critical
-          audience and to receive valuable feedback.
-        </p>
-        <ol class="items-center sm:flex">
-          <!-- MOTOGRAPH 2018 -->
-          <li class="relative mb-6 sm:mb-0">
-            <div class="flex items-center">
-              <div
-                class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0"
-              >
-                <svg
-                  class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"
-                  />
-                </svg>
-              </div>
-              <div
-                class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"
-              ></div>
-            </div>
-            <div class="mt-3 sm:pe-8">
-              <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                MOTOGRAPH 2018
-              </h3>
-              <time
-                class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
-                >Held on November 5, 2018</time
-              >
-              <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-                The inaugural MOTOGRAPH event hosted in Quebec.
-              </p>
-            </div>
-          </li>
+      <section
+        id="about"
+        class="bg-gradient-to-r from-green-500 via-green-300 to-green-100 text-center py-16"
+        aria-labelledby="about-heading"
+      >
+        <div
+          class="max-w-4xl mx-auto bg-white bg-opacity-10 backdrop-blur-lg rounded-lg shadow-lg p-8"
+        >
+          <h1
+            id="about-heading"
+            class="text-4xl font-extrabold text-gray-800 mb-6"
+          >
+            About the Event
+          </h1>
+          <p class="text-lg text-gray-700 leading-relaxed mb-6">
+            GRAPHQUON (formerly MOTOGRAPH) is the annual Quebec-Ontario
+            pre-SIGGRAPH workshop, organized by and for the major East-Canadian
+            computer graphics labs. It is an opportunity for students to present
+            their prospective submissions to a friendly, constructively critical
+            audience and to receive valuable feedback.
+          </p>
 
-          <!-- FOIEGRAPH 2022 -->
-          <li class="relative mb-6 sm:mb-0">
-            <div class="flex items-center">
-              <div
-                class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0"
-              >
-                <svg
-                  class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
+          <ol class="items-center sm:flex justify-center space-y-8 sm:space-y-0 sm:space-x-8">
+            <!-- MOTOGRAPH 2018 -->
+            <li class="relative sm:mb-0">
+              <div class="flex items-center justify-center mb-4 sm:mb-0">
+                <div
+                  class="z-10 flex items-center justify-center w-8 h-8 bg-blue-100 rounded-full ring-4 ring-white dark:bg-blue-900 dark:ring-gray-900"
                 >
-                  <path
-                    d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"
-                  />
-                </svg>
+                  <svg
+                    class="w-4 h-4 text-blue-800 dark:text-blue-300"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"
+                    />
+                  </svg>
+                </div>
               </div>
-              <div
-                class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"
-              ></div>
-            </div>
-            <div class="mt-3 sm:pe-8">
-              <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                FOIEGRAPH 2022
-              </h3>
-              <time
-                class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
-                >Held on October 17, 2022</time
-              >
-              <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-                FOIEGRAPH 2022 expanded the MOTOGRAPH tradition, hosted in
-                Ontario.
-              </p>
-            </div>
-          </li>
+              <div class="text-center sm:text-left">
+                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                  MOTOGRAPH 2018
+                </h3>
+                <time
+                  class="block mb-2 text-sm text-gray-400 dark:text-gray-500"
+                  >Held on November 5, 2018</time
+                >
+                <p class="text-base text-gray-500 dark:text-gray-400">
+                  The inaugural MOTOGRAPH event hosted in Quebec.
+                </p>
+              </div>
+            </li>
 
-          <!-- GRAPHQUON 2023 -->
-          <li class="relative mb-6 sm:mb-0">
-            <div class="flex items-center">
-              <div
-                class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0"
-              >
-                <svg
-                  class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
+            <!-- FOIEGRAPH 2022 -->
+            <li class="relative sm:mb-0">
+              <div class="flex items-center justify-center mb-4 sm:mb-0">
+                <div
+                  class="z-10 flex items-center justify-center w-8 h-8 bg-blue-100 rounded-full ring-4 ring-white dark:bg-blue-900 dark:ring-gray-900"
                 >
-                  <path
-                    d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"
-                  />
-                </svg>
+                  <svg
+                    class="w-4 h-4 text-blue-800 dark:text-blue-300"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"
+                    />
+                  </svg>
+                </div>
               </div>
-              <div
-                class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"
-              ></div>
-            </div>
-            <div class="mt-3 sm:pe-8">
-              <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                GRAPHQUON 2023
-              </h3>
-              <time
-                class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
-                >Held on November 9-10, 2023</time
-              >
-              <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-                GRAPHQUON continued to grow as the premier Quebec-Ontario
-                pre-SIGGRAPH event.
-              </p>
-            </div>
-          </li>
-        </ol>
+              <div class="text-center sm:text-left">
+                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                  FOIEGRAPH 2022
+                </h3>
+                <time
+                  class="block mb-2 text-sm text-gray-400 dark:text-gray-500"
+                  >Held on October 17, 2022</time
+                >
+                <p class="text-base text-gray-500 dark:text-gray-400">
+                  FOIEGRAPH 2022 expanded the MOTOGRAPH tradition, hosted in
+                  Ontario.
+                </p>
+              </div>
+            </li>
+
+            <!-- GRAPHQUON 2023 -->
+            <li class="relative sm:mb-0">
+              <div class="flex items-center justify-center mb-4 sm:mb-0">
+                <div
+                  class="z-10 flex items-center justify-center w-8 h-8 bg-blue-100 rounded-full ring-4 ring-white dark:bg-blue-900 dark:ring-gray-900"
+                >
+                  <svg
+                    class="w-4 h-4 text-blue-800 dark:text-blue-300"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"
+                    />
+                  </svg>
+                </div>
+              </div>
+              <div class="text-center sm:text-left">
+                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                  GRAPHQUON 2023
+                </h3>
+                <time
+                  class="block mb-2 text-sm text-gray-400 dark:text-gray-500"
+                  >Held on November 9-10, 2023</time
+                >
+                <p class="text-base text-gray-500 dark:text-gray-400">
+                  GRAPHQUON continued to grow as the premier Quebec-Ontario
+                  pre-SIGGRAPH event.
+                </p>
+              </div>
+            </li>
+          </ol>
+        </div>
       </section>
     `;
   }
